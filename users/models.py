2fa2afc084418db1f01b0ddf5b0c843c651b9d92
Roman Ivanov <sitdoff@gmail.com>
2023-04-21 11:06:46 +0300

    add utils.py, update CustomUser.photo field

--- users/models.py
+++ users/models.py
@@ -1,11 +1,13 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from .utils import user_directory_path
+
 # Create your models here.
 
 
 class CustomUser(AbstractUser):
-    photo = models.ImageField(upload_to="userpic/%Y %m %d/", blank=True, verbose_name="Фотография")
+    photo = models.ImageField(upload_to=user_directory_path, blank=True, verbose_name="Фотография")
     bio = models.TextField(blank=True, verbose_name="О себе")
     is_author = models.BooleanField(default=False, verbose_name="Статус автора")
 
