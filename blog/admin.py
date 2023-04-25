39b677a47a940b7f26473833a11cdc2b95ced99c
Roman Ivanov <sitdoff@gmail.com>
2023-04-25 13:28:42 +0300

    fix PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -8,7 +8,7 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = "Пусто"
     readonly_fields = ("time_create", "time_update")
     fields = ("title", "slug", "epigraph", "article", "author", "image", "time_create", "time_update", "is_published")
-    list_display = ("title", "slug", "article", "image", "time_create", "time_update", "is_published")
+    list_display = ("title", "slug", "author", "article", "image", "time_create", "time_update", "is_published")
     prepopulated_fields = {"slug": ("title",)}
 
 
