4433654f33a7b41b86ca5c079562912b1e0eff8f
Roman Ivanov <sitdoff@gmail.com>
2023-04-26 06:31:19 +0300

    add auth_menu tag

--- users/views.py
+++ users/views.py
@@ -10,12 +10,12 @@ from .forms import RegisterUserForm
 # Create your views here.
 class RegisterUser(CreateView):
     form_class = RegisterUserForm
-    template_name = "blog/register.html"
+    template_name = "users/register.html"
     success_url = reverse_lazy("home")
 
 
 class LoginUser(LoginView):
-    template_name = "blog/login.html"
+    template_name = "users/login.html"
     next_page = reverse_lazy("home")
 
 
