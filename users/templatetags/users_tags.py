84898f01c4d7745eba481a458e49cff57ec5a318
Roman Ivanov <sitdoff@gmail.com>
2023-04-28 09:07:05 +0300

    add author_info tag

--- users/templatetags/users_tags.py
+++ users/templatetags/users_tags.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.inclusion_tag("users/auth_menu.html")
 def auth_menu(user):
     return {"user": user}
+
+
+@register.inclusion_tag("users/author_info.html")
+def author_info(author):
+    return {"author": author}
