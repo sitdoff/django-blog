27195c0e2d2349694c039d9574e07d44ddfd6fe5
Roman Ivanov <sitdoff@gmail.com>
2023-05-26 08:43:02 +0300

    Add tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,58 @@
+from django.shortcuts import reverse
 from django.test import TestCase
 
+from blog.models import Post
+from users.models import CustomUser
+
 # Create your tests here.
+
+
+class CreateTestUsersAndPostsMixin:
+    users = [
+        {"username": "user", "email": "user@test.com"},
+        {"username": "author", "email": "author@test.com", "is_author": True},
+        {"username": "staff", "email": "staff@mail.com", "is_staff": True},
+        {"username": "authorstaff", "email": "authorstaff@test.com", "is_author": True, "is_staff": True},
+        {"username": "admin", "email": "admin@test.com", "is_superuser": True},
+    ]
+    posts = {
+        "draft": {"title": "draft_post", "is_draft": True, "is_published": False},
+        "unpublished": {"title": "unpublished_post", "is_draft": False, "is_published": False},
+        "published": {"title": "published_post", "is_draft": False, "is_published": True},
+    }
+
+    def setUp(self):
+        """Create users and posts"""
+
+        self.test_users = [None]
+        self.test_posts = []
+
+        for user_data in self.users:
+            user = CustomUser.objects.create(**user_data)
+            self.test_users.append(user)
+
+        author = CustomUser.objects.get(username="author")
+        for post_data in self.posts:
+            post = Post.objects.create(author=author, **self.posts[post_data])
+            self.test_posts.append(post)
+
+
+class TestBlog(CreateTestUsersAndPostsMixin, TestCase):
+    pages = {
+        "home": {"url": "/", "kwargs": {}},
+        "about": {"url": "/about", "kwargs": {}},
+        "gallery": {"url": "/gallery", "kwargs": {}},
+        "contact": {"url": "/contact", "kwargs": {}},
+        "register": {"url": "/user/register", "kwargs": {}},
+        "login": {"url": "/user/login", "kwargs": {}},
+        "author_posts": {"url": "/user/author/author", "kwargs": {"username": "author"}},
+        "post": {"url": "/post/published-post", "kwargs": {"post_slug": "published-post"}},
+    }
+
+    def test_pages(self):
+        for url_name in self.pages:
+            url = reverse(url_name, kwargs=self.pages[url_name]["kwargs"])
+            response = self.client.get(url)
+            self.assertEqual(response.request["PATH_INFO"], self.pages[url_name]["url"])
+            self.assertEqual(response.status_code, 200, f"\n\nUrl is {url}.\nStaus-code is {response.status_code}")
+            print(f'Page "{url}" OK')
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,56 @@
+from django.shortcuts import reverse
 from django.test import TestCase
 
+from blog.models import Post
+from blog.tests import CreateTestUsersAndPostsMixin
+from users.models import CustomUser
+
 # Create your tests here.
+# Users for tests
+
+
+class TestPermissions(CreateTestUsersAndPostsMixin, TestCase):
+    """Test pages with permissions"""
+
+    # key - url name
+    # value - access
+    pages = {
+        "add_post": {"access": ("author", "authorstaff", "admin"), "kwargs": {}},
+        "drafts": {"access": ("author", "authorstaff", "admin"), "kwargs": {}},
+        "unpublished_posts": {"access": ("staff", "authorstaff", "admin"), "kwargs": {}},
+        "draft": {"access": ("author", "admin"), "kwargs": {"post_slug": "draft-post"}},
+        "edit_draft": {"access": ("author", "admin"), "kwargs": {"post_slug": "draft-post"}},
+        "unpublished_post": {"access": ("staff", "authorstaff", "admin"), "kwargs": {"post_slug": "unpublished-post"}},
+        "edit_post": {"access": ("staff", "authorstaff", "admin"), "kwargs": {"post_slug": "unpublished-post"}},
+        "profile_edit": {"access": ("author", "authorstaff", "admin"), "kwargs": {}},
+    }
+
+    def test_permissions(self):
+        """Main test method"""
+
+        for page in self.pages:
+            url = reverse(page, kwargs=self.pages[page]["kwargs"])
+            for user in self.test_users:
+                self.page_test(user=user, url=url, access=self.pages[page]["access"])
+            print(f'Permissions page "{url}" OK')
+
+    def page_test(self, user=None, url=None, access=None):
+        """Request method"""
+
+        self.client.logout()
+        if user:
+            self.client.force_login(user)
+        response = self.client.get(url)
+        if not user:
+            self.assertEqual(
+                response.status_code, 302, f"\n\nUrl: '{url}'.\nUser: {user}.\nStatus code: {response.status_code}"
+            )
+            self.assertRedirects(response, "/user/login")
+        elif user.username in access:
+            self.assertEqual(
+                response.status_code, 200, f"\n\nUrl: '{url}'.\nUser: {user}.\nStatus code: {response.status_code}"
+            )
+        else:
+            self.assertEqual(
+                response.status_code, 403, f"\n\nUrl: '{url}'.\nUser: {user}.\nStatus code: {response.status_code}"
+            )
