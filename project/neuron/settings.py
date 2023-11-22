77af89ab521211cec7d5ae50e7236ab4f6d8a322
Roman Ivanov <sitdoff@gmail.com>
2023-11-22 09:59:49 +0300

    add settings for good typing

--- project/neuron/settings.py
+++ project/neuron/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+import django_stubs_ext
 from dotenv import load_dotenv
 
 from .ckeditor_config import CKEDITOR_CONFIGS
+
+django_stubs_ext.monkeypatch()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
