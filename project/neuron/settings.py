51bbec525529c548ec5540670ae4c6aa66383448
Roman Ivanov <sitdoff@gmail.com>
2024-02-08 08:51:55 +0300

    add FLower for Celery

--- project/neuron/settings.py
+++ project/neuron/settings.py
@@ -173,8 +173,8 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Celery settings
 
-CELERY_BROKER_URL = "redis://redis:6379/0"
-CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_TIMEZONE = "Europe/Moscow"
