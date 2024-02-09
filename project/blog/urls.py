ba6f0f486f15832de401b1e7116d3ea121bbbc52
Roman Ivanov <sitdoff@gmail.com>
2024-02-09 09:11:22 +0300

    update blog/urls.py

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("post/draft/<slug:post_slug>", views.DraftPostDetailView.as_view(), name="draft"),
     path("drafts/edit/<slug:post_slug>", views.EditDraftPost.as_view(), name="edit_draft"),
     path("unpublished", views.UnpublishedPostsView.as_view(), name="unpublished_posts"),
+    path("unpublished/set_editor", views.set_editor, name="set_editor"),
     path("unpublished/<slug:post_slug>", views.UnpublishedPostDetailView.as_view(), name="unpublished_post"),
     path("unpublish/edit/<slug:post_slug>", views.EditUnpublishedPost.as_view(), name="edit_post"),
 ]
