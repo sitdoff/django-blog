40864c4a9ac5916d4610aa529d0e763d6119909c
Roman Ivanov <sitdoff@gmail.com>
2024-03-15 08:40:50 +0300

    add unsubscribe link

--- project/blog/static/blog/js/custom.js
+++ project/blog/static/blog/js/custom.js
@@ -56,62 +56,42 @@ document.addEventListener("DOMContentLoaded", function() {
   setTimeout(clickButtons, 5000);
 });
 
-/* Subscrib
-  -----------------------------------------------*/
-// ajax.js
-
 $(document).ready(function() {
-  $("#subscribe-link").one("click", function(event) {
+  // Функция, которая будет проверять наличие контейнера с классом "message-container fixed-top"
+  function getMessageContainer() {
+    var existingContainer = document.querySelector(".message-container.fixed-top");
+    if (!existingContainer) {
+      existingContainer = document.createElement("div");
+      existingContainer.className = "message-container fixed-top";
+      document.body.appendChild(existingContainer);
+    }
+    return existingContainer;
+  }
+
+  $(".subscribe-unsubscribe").on("click", "#subscribe-link", function(event) {
     event.preventDefault(); // Предотвращаем стандартное действие ссылки
 
     var link = this;
 
     // Получаем URL из атрибута data-url
-    var url = $(this).data("url");
+    var url = link.getAttribute("data-url");
 
     // Выполняем AJAX запрос
     $.ajax({
       url: url, // URL представления
       method: "GET", // Метод запроса (GET, POST и т.д.)
       success: function(data) {
-        // Меняем текс в ссылке
+        // Меняем текст в ссылке
         if (link.textContent == "Подписаться") {
-          link.textContent = "Вы подписаны";
-          link.id = "none";
-          $(link).attr("href", "###");
-          link.setAttribute("data-url", "#");
+          link.textContent = "Отписаться";
+          link.id = "unsubscribe-link";
+          var unsubscribeUrl = url.replace("subscribe", "unsubscribe");
+          link.setAttribute("data-url", unsubscribeUrl);
         } else {
-          link.textContent = "Вы подписаны";
+          link.textContent = "Подписаться";
         }
 
-        // Создаем родительский контейнер для сообщений и присваиваем ему класс
-        var messageDivParent = document.createElement("div");
-        messageDivParent.className = "message-container fixed-top";
-
-        // Добавляем родителский класс в документ
-        document.body.appendChild(messageDivParent);
-
-        // Создаем дочерний класс сообщений и присваиваем ему id, класс и роль
-        var messageDiv = document.createElement("div");
-        messageDiv.id = "message-div";
-        messageDiv.className = "alert " + data.message_level + " alert-dismissible";
-        messageDiv.role = "alert";
-
-        // Добавляем дочерний класс в родительский
-        messageDivParent.appendChild(messageDiv);
-
-        // Получаем текст сообщения
-        var message = data.message;
-        // Изменяем содержимое div на сообщение
-        $("#message-div").text(message);
-        // Показываем div
-        $("#message-div").show();
-
-        setTimeout(function() {
-          // Скрываем и удаляем элемент
-          messageDivParent.style.display = "none";
-          messageDivParent.remove();
-        }, 2000); // 2000 миллисекунд = 2 секунды
+        showMessage(data.message, data.message_level);
       },
       error: function(xhr, status, error) {
         // Обработка ошибки
@@ -119,4 +99,59 @@ $(document).ready(function() {
       },
     });
   });
+
+  $(".subscribe-unsubscribe").on("click", "#unsubscribe-link", function(event) {
+    event.preventDefault(); // Предотвращаем стандартное действие ссылки
+
+    var link = this;
+
+    // Получаем URL из атрибута data-url
+    var url = link.getAttribute("data-url");
+
+    // Выполняем AJAX запрос
+    $.ajax({
+      url: url, // URL представления
+      method: "GET", // Метод запроса (GET, POST и т.д.)
+      success: function(data) {
+        // Меняем текст в ссылке
+        if (link.textContent == "Отписаться") {
+          link.textContent = "Подписаться";
+          link.id = "subscribe-link";
+          var subscribeUrl = url.replace("unsubscribe", "subscribe");
+          link.setAttribute("data-url", subscribeUrl);
+        } else {
+          link.textContent = "Отписаться";
+        }
+
+        showMessage(data.message, data.message_level);
+      },
+      error: function(xhr, status, error) {
+        // Обработка ошибки
+        console.error("Произошла ошибка:", error);
+      },
+    });
+  });
+
+  function showMessage(message, messageLevel) {
+    // Создаем родительский контейнер для сообщений и присваиваем ему класс
+    var messageDivParent = getMessageContainer();
+
+    // Создаем дочерний класс сообщений и присваиваем ему id, класс и роль
+    var messageDiv = document.createElement("div");
+    messageDiv.id = "message-div";
+    messageDiv.className = "alert " + messageLevel + " alert-dismissible";
+    messageDiv.role = "alert";
+    messageDiv.textContent = message;
+
+    // Добавляем дочерний класс в родительский
+    messageDivParent.appendChild(messageDiv);
+
+    // Показываем div
+    $("#message-div").show();
+
+    setTimeout(function() {
+      // Скрываем и удаляем элемент
+      $("#message-div").remove();
+    }, 2000); // 2000 миллисекунд = 2 секунды
+  }
 });
